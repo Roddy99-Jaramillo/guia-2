a69b2d3143af5b37e7d18a0e484f5c04c65b1c6d
Cristopher99-loyo <52213209+Cristopher99-loyo@users.noreply.github.com>
2020-01-08 09:25:14 -0500

    Update guia-practica2.cpp
    modificar

--- guia-practica2.cpp
+++ guia-practica2.cpp
@@ -2,44 +2,47 @@
 #include <stdlib.h>
 #include <pthread.h> 
 #include <unistd.h> 
-#include<windows.h>
+ #include<windows.h>
 #include <string.h>
+#include <time.h>
+int cambiarColor( int semaforo )
+{
+if( semaforo == 1 )
+{
+semaforo = 0; 
+}
+else
+{
+semaforo = 1;
+}
+return semaforo;
+} 
+int cambiarColor( int );
 void *verde(void*); 
 void *naranja(void*); 
 void *rojo(void*);
+	int verdes;
+	int amarrillo;
+	int rojos;
 void *verde(void *tid)
 {
+printf( "Introduzca el tiempo del color Verde" );
+scanf( "%d", &verdes);
+	printf( "Introduzca el tiempo del color Amarillo" ); 
+scanf( "%d", &amarrillo);
+	printf( "Introduzca el tiempo del color Rojo" ); 
+scanf( "%d", &rojos);
 system("cls"); 
 system("color A0"); 
-Sleep(3000);
+Sleep(verdes);
 }
 
 void *naranja(void *tid)
 {
 system("cls"); 
 system("color E0"); 
-Sleep(3000);
+Sleep(amarrillo);
 }
-void *rojo(void *tid)
-{
-system("cls"); 
-system("color 40");
- Sleep(3000);
-}
-int main()
-{
-int i = 1;
-pthread_t pthread_rojo, pthread_naranja, pthread_verde;
 
-pthread_create (&pthread_verde, NULL, verde, (void *)i); pthread_join(pthread_verde, NULL);
-
-pthread_create (&pthread_naranja,NULL, naranja, (void *)i); pthread_join(pthread_naranja, NULL);
-
-pthread_create (&pthread_rojo, NULL, rojo, (void *)i); pthread_join(pthread_rojo, NULL);
-
-pthread_exit(NULL);
-
-return 0;
-}
 
 
